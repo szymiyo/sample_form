6a9ebd5f21e8417f23d6a7300bb6281e20a13328
szymonn7 <szymon@swiderscy.com>
2021-10-08 12:25:02 +0200

    Complete form

--- js/validation.js
+++ js/validation.js
@@ -9,3 +9,75 @@
     Powodzenia :)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
+
+// Definicja walidacji sprawdzajaca czy imie sklada sie tylko z liter
+jQuery.validator.addMethod("just_letters_name", function(value, element) {
+    return this.optional(element) || /^[A-Za-z]+$/.test(value);
+}, "Twoje imię zawiera niedozwolony znak.");
+
+// Definicja walidacji sprawdzajaca czy nazwisko sklada sie tylko z liter
+jQuery.validator.addMethod("just_letters_surname", function(value, element) {
+    return this.optional(element) || /^[A-Za-z]+$/.test(value);
+}, "Twoje nazwisko zawiera niedozwolony znak.");
+
+$(document).ready(function() {
+    $("#basic-form").validate({
+      rules: {
+        name : {
+            // Pole nie może być puste
+            required: true,
+            // Minimalna ilość znaków wynosi 3
+            minlength: 3,
+            // Wykorzystanie zdefiniowanej walidacji
+            just_letters_name: true
+        },
+        surname: {
+            // Pole nie może być puste
+            required: true,
+            // Minimalna ilość znaków wynosi 3
+            minlength: 3,
+            // Wykorzystanie zdefiniowanej walidacji
+            just_letters_surname: true
+        },
+        phone : {
+            // Pole nie może byćpuste
+            required: true,
+            // Pole może zawierać tylko cyfry
+            pattern: /^[0-9]+$/
+        },
+        email: {
+            // Pole wymagane
+            required: true,
+            email: true
+        },
+      },
+      //Wiadomości walidacji
+      messages : {
+        name: {
+            required: "Pole nie może być puste.",
+            minlength: "Imię musi zawierać co najmniej 3 znaki."
+        },
+        surname: {
+            required: "Pole nie może być puste.",
+            minlength: "Nazwisko musi zawierać co najmniej 3 znaki."
+        },
+        phone: {
+            required: "Pole nie może być puste.",
+            pattern: "Zły format numeru telefonu."
+        },
+        email: {
+            required: "Pole nie może być puste.",
+            email: "Zły format e-mail."
+        },
+      }
+    });
+  });
+
+  // Funkcja sprawdzająca, czy przynajmniej 1 checkbox jest zaznaczony
+  $("#basic-form").submit(function(){
+    var checked = $("#basic-form input:checked").length > 0;
+    if (!checked){
+        alert("Please check at least one checkbox");
+        return false;
+    }
+});
